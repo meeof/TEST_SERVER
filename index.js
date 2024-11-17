fffad12c2f79cdb0ce25acd73ddc779a53a3b78e
meeof <148491136+meeof@users.noreply.github.com>
2024-11-17 09:49:47 +0300

    server

--- index.js
+++ index.js
@@ -3,6 +3,6 @@ let app = express();
 app.get('/', function(req, res) {
   res.send('hello world');
 });
-app.listen(5000, function() {
+app.listen(5000, '176.114.70.30', function() {
   console.log('running');
 });
